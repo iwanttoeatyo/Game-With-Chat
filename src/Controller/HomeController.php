6f3a7411180448eb40c8fd7865b24760216de2bb
Chris Boyd <boydc@urgame.me>
2017-03-10 16:01:57 -0600

    started on loading messages for home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Cake\Event\Event;
 /**
  * Home Controller
  *
+ * @property \App\Model\Table\MessagesTable $Messages
  * @property \App\Model\Table\HomeTable $Home
  */
 class HomeController extends AppController
@@ -23,7 +24,21 @@ class HomeController extends AppController
     public function index()
     {
     	$title = '';
-    	$this->set(compact('title'));
+		$this->set(compact('title'));
+		$chat_id = 1;
+		//todo get recent messages
+		$messages = $this->$Messages->find('all')
+			->where(['Messages.date_created >' => new DateTime('-1') ])
+			->where(['chat_id' => $chat_id])
+			->order(['date_created'] => 'ASC')
+			->limit(10);
+
+		//todo get playerlist
+		//todo get lobby list
+
+
+
+
 		$this->render('index');
     }
 
