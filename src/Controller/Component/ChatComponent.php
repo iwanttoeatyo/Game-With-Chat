6fe3d009f13634f7b1ca2361a2c27e05e29ac1de
Chris Boyd <boyd111c@gmail.com>
2017-03-12 03:19:15 -0600

    message order

--- src/Controller/Component/ChatComponent.php
+++ src/Controller/Component/ChatComponent.php
@@ -3,6 +3,7 @@ namespace App\Controller\Component;
 
 use Cake\Controller\Component;
 use Cake\Controller\ComponentRegistry;
+use Cake\Database\Query;
 use Cake\ORM\TableRegistry;
 
 /**
@@ -15,22 +16,29 @@ class ChatComponent extends Component
 	{
 		parent::initialize($config);
 		$this->Messages = TableRegistry::get('Messages');
-
 	}
 
-    /**
-     * Default configuration.
-     *
-     * @var array
-     */
-    protected $_defaultConfig = [];
+	/**
+	 * Default configuration.
+	 *
+	 * @var array
+	 */
+	protected $_defaultConfig = [];
 
-    public function getMessages($chat_id){
-		$messages = $this->Messages->find()
+	public function getMessages($chat_id)
+	{
+		$results = $this->Messages->find()
 			->where(['chat_id ' => $chat_id])
-			->order(['created_date' => 'ASC'])
+			->order(['created_date' => 'DESC'])
 			->limit(10)
 			->all();
+
+		$messages = [];
+		foreach ($results as $message){
+			$messages[] = $message;
+		}
+		$messages = array_reverse($messages);
+
 		return $messages;
 	}
 }
